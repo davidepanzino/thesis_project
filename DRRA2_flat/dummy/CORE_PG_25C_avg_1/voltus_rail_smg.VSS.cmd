1e9f04a2d31c5747d04563717cc86c888f5fc42e
Davide Finazzi <finazzi.davide01@outlook.it>
2025-06-25 16:29:33 +0000

    Add rail results flat

---
+++ DRRA2_flat/dummy/CORE_PG_25C_avg_1/voltus_rail_smg.VSS.cmd
@@ -0,0 +1,32 @@
+{
+    "nets": {
+        "VSS": {
+            "name": "VSS",
+            "output_path": ".\/\/CORE_PG_25C_avg_1\/VSS",
+            "nominal_voltage": "0"
+        }
+    },
+    "cellview_file": ".\/work\/VSS.cell.views",
+    "temporary_directory": "\/home\/finazzi\/Desktop\/9_june_flat\/dummy\/innovus_temp_2204989_f202f6cf-7bde-47fb-804a-3a6fb97a0d6d_4f5323946af0_finazzi_TiJwh4\/",
+    "pgdb_path": ".\/work\/",
+    "use_def_pins": "true",
+    "sfedb_path": ".\/work\/SFEDB",
+    "thread_num": "4",
+    "cell_lib_path": ".\/techonly.cl",
+    "temperature": "25",
+    "accuracy_mode": "hd",
+    "extraction_backend": "zx",
+    "unified_power_switch_flow": {
+        "enable": "true",
+        "net_meta_file": ".\/work\/net_meta.json"
+    },
+    "force_power_gates_on": "false",
+    "disallow_tolerance": "false",
+    "disallow_via_tolerance": "true",
+    "tolerance_variated_side": "2",
+    "tolerance_fixed_side": "2",
+    "tolerance_partial_variated_side": "2",
+    "tolerance_partial_fixed_side": "2",
+    "small_segment_length": "2",
+    "dump_partial_match_info": "true"
+}
